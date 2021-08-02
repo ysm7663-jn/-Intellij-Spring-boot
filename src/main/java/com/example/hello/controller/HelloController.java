a76098eb7862c9950d74b4dcf1bc2fb30020388e
ysm7663-jn <ysm7663@gmail.com>
2021-08-02 17:42:58 +0900

    동적(MVC 패턴)

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,19 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // @ResponseBody : http의 body부에 직접 return 내용을 직접 넣겠다.
+    // view가 필요없음
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
