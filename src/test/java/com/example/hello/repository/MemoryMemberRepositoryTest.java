100ae03bf8b97bba7184d9c8239549d8d489209c
ysm7663-jn <ysm7663@gmail.com>
2021-08-09 14:46:36 +0900

    MemoryMemberRepository Test 수행

---
+++ src/test/java/com/example/hello/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.hello.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+
+    }
+
+}
