3137252be78718c75073f75c31daa0d5a5b29342
ysm7663-jn <ysm7663@gmail.com>
2021-08-10 20:03:58 +0900

    Member 추가 설명

--- src/main/java/com/example/hello/repository/MemberRepository.java
+++ src/main/java/com/example/hello/repository/MemberRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
