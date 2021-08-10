a36c5da0332b6d6f095118015d1822913ab92963
ysm7663-jn <ysm7663@gmail.com>
2021-08-10 20:03:27 +0900

    Member 추가 설명

--- src/main/java/com/example/hello/service/MemberService.java
+++ src/main/java/com/example/hello/service/MemberService.java
@@ -3,14 +3,22 @@ package com.example.hello.service;
 import com.example.hello.domain.Member;
 import com.example.hello.repository.MemberRepository;
 import com.example.hello.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
+// @Autowired를 사용하기 위해서 작성
+// springcontainer에 memberService를 넣어놓는다
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
+    // Service와 Repository를 연결
+    // 스프링 컨테이너에 존재하는 MemoryMemberRepository를 주입해 줌
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
