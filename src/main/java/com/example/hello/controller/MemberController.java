59df8006763a876c6b8687c579e191f005ecd168
ysm7663-jn <ysm7663@gmail.com>
2021-08-10 20:03:24 +0900

    Member 추가 설명

---
+++ src/main/java/com/example/hello/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.example.hello.controller;
+
+import com.example.hello.domain.Member;
+import com.example.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    // Service에 @Service
+    // Repository에 @Repository
+    // Controller 와 Service를 연결 : DI(스프링이 의존관계를 주입)
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
