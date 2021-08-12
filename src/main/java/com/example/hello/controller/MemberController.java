431514d64a5c37bd90ac1e489ec6e91b36a60fb6
ysm7663-jn <ysm7663@gmail.com>
2021-08-12 16:05:45 +0900

    MVC - form을 활용한 데이터 저장

--- src/main/java/com/example/hello/controller/MemberController.java
+++ src/main/java/com/example/hello/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.example.hello.domain.Member;
 import com.example.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // MemberController -> MemberService -> MemberRepository
 
@@ -37,5 +39,20 @@ public class MemberController {
     // public void setMemberService(MemberService memberService) {
     //      this.memberService = memberService;
     //  }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 }
