6e03de692e2957941f4f53bfd701b22e451a1230
ysm7663-jn <ysm7663@gmail.com>
2021-08-12 17:12:48 +0900

    MVC - 저장된 데이터를 List형태로 보여주기

--- src/main/java/com/example/hello/controller/MemberController.java
+++ src/main/java/com/example/hello/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.hello.domain.Member;
 import com.example.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // MemberController -> MemberService -> MemberRepository
 
@@ -53,6 +56,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
