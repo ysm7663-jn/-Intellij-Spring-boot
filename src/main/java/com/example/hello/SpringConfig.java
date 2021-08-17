3e7d6841ab53d9b4f3fb2fd9639d429cb2705357
ysm7663-jn <ysm7663@gmail.com>
2021-08-17 19:12:07 +0900

    SpringConfig

--- src/main/java/com/example/hello/SpringConfig.java
+++ src/main/java/com/example/hello/SpringConfig.java
@@ -1,10 +1,15 @@
 package com.example.hello;
 
+import com.example.hello.repository.JdbcMemberRepository;
 import com.example.hello.repository.MemberRepository;
 import com.example.hello.repository.MemoryMemberRepository;
 import com.example.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 // spring bean: 상황에 따라 구현 클래스를 변경해야 하는 경우 유리
 
@@ -13,6 +18,15 @@ import org.springframework.context.annotation.Configuration;
 // ∴ spring bean에 등록된 것만이 @Autowired가 적용된다.
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    // spring bean이 application.properties에서 설정을 확인하고 직접 생성해줌
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     // 스프링 빈에 @Bean을 등록
     @Bean
     public MemberService memberService() {
@@ -21,7 +35,11 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // JDBC 사용전
+        // return new MemoryMemberRepository();
+
+        // 확장
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
